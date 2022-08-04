a879a77f36e21662c1097e59c6bfcfe543abf3c8
Frank Naw <franknaw@gmail.com>
2022-08-04 10:44:41 -0400

    Update

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+//
+// app.use('/', express.static(__dirname + '/html'));
+// app.get('/', function (req, res) {
+//     res.render('index.html');
+// });
+//
+// app.listen(8080, () => {
+//     console.log('Listening on port 8080');
+// });
 
-app.use('/', express.static(__dirname + '/html'));
-app.get('/', function (req, res) {
-    res.render('index.html');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World22!");
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
-});
+const port = process.env.PORT || 1337;
+server.listen(port);
 
+console.log("Server running at http://localhost:%d", port);
