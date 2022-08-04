efcb3be8f5ce83947a8b8e8ece8c69bbae8e8508
Frank Naw <franknaw@gmail.com>
2022-08-04 15:43:32 -0400

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = express();
-//
-// app.use('/', express.static(__dirname + '/html'));
-// app.get('/', function (req, res) {
-//     res.render('index.html');
-// });
-//
-// app.listen(8080, () => {
-//     console.log('Listening on port 8080');
-// });
-
 const http = require('http');
 
 const server = http.createServer((request, response) => {
