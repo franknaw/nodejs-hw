9f62a74ee21ab0b42af1c80e100d535feb717b4d
Frank Naw <franknaw@gmail.com>
2022-08-06 08:57:40 -0400

    Update port env

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 1337;
 
 app.use('/', express.static(__dirname + '/html'));
 app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
 });
